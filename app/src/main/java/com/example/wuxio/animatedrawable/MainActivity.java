b81955d6fdf73474540b52229697062689838310
threekilogram <wuxiongc@163.com>
2018-05-12 11:02:35 +0800

    初始化项目

---
+++ app/src/main/java/com/example/wuxio/animatedrawable/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.wuxio.animatedrawable;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author wuxio
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
